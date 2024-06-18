a5fcb3886d0e71ee466933881dcc6969acb272e5
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-18 11:12:04 -0400

    yes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+# take python object and convert to JSON data & vice-versa;
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Userfields = ["id", "username", "password"]
