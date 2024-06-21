4eb737a3da2e808415d05e0a29eeb74c52439740
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-21 08:38:14 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/djangoreactnoteapp/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, // import anything specified in an env variable file.
